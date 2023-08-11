6d2d85cf36a0a29c2263ba384abe524053f044f1
kitou88C39 <kanfutrooper@icloud.com>
2023-08-11 16:35:24 +0900

    first commit

---
+++ src/test/java/com/liso/springboot/Restful/SpringbootRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liso.springboot.Restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
