744a01cd39f6f4ad000face93ad02834201a4c4e
kitou88C39 <kanfutrooper@icloud.com>
2023-08-20 11:47:12 +0900

    entities/City.javaを編集

---
+++ src/main/java/com/liso/springboot/entities/City.java
@@ -0,0 +1,34 @@
+package com.liso.springboot.entities;
+
+import javax.persistence.Table;
+
+import jakarta.annotation.Generated;
+
+@Entity
+@Table(name = "cities")
+public class City {
+    
+    @Id
+    @Column(name = "city_id")
+    @GeneratedValue(strategy - GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "city_name")
+    private String cityname;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+    
+    public String getCityname(){
+        return cityname;
+    }
+
+    public void setCityname(String cityname){
+        this.cityname = cityname;
+    }
+}
