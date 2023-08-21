67f52c8abe533c5ca8af9c9362e64121ffe9fb75
kitou88C39 <kanfutrooper@icloud.com>
2023-08-21 09:04:33 +0900

    repositories/CityRepository.javaを編集

--- src/main/java/com/liso/springboot/repositories/CityRepository.java
+++ src/main/java/com/liso/springboot/repositories/CityRepository.java
@@ -1,6 +1,9 @@
 package com.liso.springboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.repository;
+
+@Respostory
 public interface CityRepository extends JpaRepository<City,Long>{
     
 }
