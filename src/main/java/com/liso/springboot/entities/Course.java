e3d4fea85c790f621b7a159a21cbc7ca9c583052
kitou88C39 <kanfutrooper@icloud.com>
2023-08-30 10:39:36 +0900

    entities/Course.javaを編集

--- src/main/java/com/liso/springboot/entities/Course.java
+++ src/main/java/com/liso/springboot/entities/Course.java
@@ -1,12 +1,13 @@
 package com.liso.springboot.entities;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 
 @Entity
 @Table(name = "courses")
@@ -17,21 +18,25 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    praivate String coursename;
+    private String coursename;
 
-    public int getId(){
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "course_coursecontents_mapping", joinColumns = @JoninColumn(name = "course_id"), inverseJoinColumns = @JoinColumn(name = "content_id"))
+    private Set<CourseContents> coursecontents;
+
+    public int getId() {
         return id;
     }
 
-    public void setId(int id){
+    public void setId(int id) {
         this.id = id;
     }
 
-    public String getCoursename(){
+    public String getCoursename() {
         return coursename;
     }
 
-    public void setCoursename(String coursename){
+    public void setCoursename(String coursename) {
         this.coursename = coursename;
     }
 }
