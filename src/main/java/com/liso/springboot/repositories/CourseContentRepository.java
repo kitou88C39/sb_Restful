427876a7fb8660b93ffc3ba48b216ae9af53c32c
kitou88C39 <kanfutrooper@icloud.com>
2023-08-31 20:29:14 +0900

    repositories/CourseContentRepository.javaを編集

--- src/main/java/com/liso/springboot/repositories/CourseContentRepository.java
+++ src/main/java/com/liso/springboot/repositories/CourseContentRepository.java
@@ -2,7 +2,9 @@ package com.liso.springboot.repositories;
 
 import com.liso.springboot.entities.CourseContents;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseContentRepository extends JpaRepository<CourseContents, Integer> {
 
     public CourseContents findById(int id);
