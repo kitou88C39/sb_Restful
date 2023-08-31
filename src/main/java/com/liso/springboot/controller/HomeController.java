e4ef635cbb7e441a087d87d00e99361a8548522a
kitou88C39 <kanfutrooper@icloud.com>
2023-08-31 20:44:19 +0900

    controller/HomeController.javaを編集

--- src/main/java/com/liso/springboot/controller/HomeController.java
+++ src/main/java/com/liso/springboot/controller/HomeController.java
@@ -1,41 +1,45 @@
 package com.liso.springboot.controller;
 
+import java.util.List;
+
+import com.liso.springboot.Pojos.CityRequest;
+import com.liso.springboot.entities.City;
+import com.liso.springboot.services.CityService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class HomeController {
-    
+
     @Autowired
     CityService cityService;
-    
+
     @GetMapping("sayhello")
-    public String sayHello(){
+    public String sayHello() {
         return "Hello User";
     }
-    
+
     @GetMapping("getcities")
-    public List<City> getCities(){
+    public List<City> getCities() {
         return cityService.getCities();
     }
 
     @PostMapping("addcity")
-    public City SaveCity(@RequestBody City city){
+    public City SaveCity(@RequestBody City city) {
         return cityService.saveCity(city);
     }
 
     @GetMapping("getcity")
-    public City getCity(String cityname){
+    public City getCity(String cityname) {
         return cityService.getCity(cityname);
     }
 
     @PostMapping("savecity")
-    public City addCity(@RequestBody CityRequest cityrequest){
-         return cityService.addCity(cityrequest);
+    public City addCity(@RequestBody CityRequest cityrequest) {
+        return cityService.addCity(cityrequest);
     }
 }
