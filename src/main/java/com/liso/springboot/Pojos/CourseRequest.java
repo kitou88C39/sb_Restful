e6ecc6aa0e90bde26f7b7a58fb68f76ba84d0bf4
kitou88C39 <kanfutrooper@icloud.com>
2023-09-01 08:46:59 +0900

    Pojos/CourseRequest.javaを作成

---
+++ src/main/java/com/liso/springboot/Pojos/CourseRequest.java
@@ -0,0 +1,13 @@
+package com.liso.springboot.Pojos;
+
+import com.liso.springboot.entities.CourseContents;
+
+public class CourseRequest {
+
+    public int id;
+
+    public String coursename;
+
+    public Set<CourseContents> coursecontents;
+
+}
