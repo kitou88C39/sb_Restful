289949d0cd7c23870626c4fed2cd3286df16b65a
kitou88C39 <kanfutrooper@icloud.com>
2023-09-01 09:41:44 +0900

    repositories/CourseContentRepository.javaを編集

--- src/main/java/com/liso/springboot/services/CourseService.java
+++ src/main/java/com/liso/springboot/services/CourseService.java
@@ -1,20 +1,39 @@
 package com.liso.springboot.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import static org.junit.jupiter.api.DynamicTest.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.liso.springboot.Pojos.CourseRequest;
+import com.liso.springboot.entities.Course;
+import com.liso.springboot.entities.CourseContents;
 import com.liso.springboot.repositories.CourseRepository;
+import org.springframework.stereotype.Service;
 
 @Service
 public class CourseService {
     @Autowired
     CourseRepository courseRepository;
 
+    @Autowired
+    CourseRepository coursecontentRepo;
+
     public CourseService() {
     }
 
-    public Course addCourseWithContents() {
+    public Course addCourseWithContents(CourseRequest courseRequest) {
         Course course = new Course();
+        course.setId(courseRequest.id);
+        course.setCoursename(courseRequest.coursename);
+        course.setCoursecontents(courseRequest.coursecontents
+        .stream()
+        .map(coursecontent -> {
+            CourseContents contents = coursecontent;
+            if(contents.getId() > 0){
+               contents = coursecontentRepo.findById(contents.getId());
+            }
+            contents.getCourses().add(course)
+        }))
 
         return courseRepository.save(course);
     }
