16baa1dc10231a0aab1df9bc9e18d7310146b5cd
kitou88C39 <kanfutrooper@icloud.com>
2023-09-04 13:52:25 +0900

    controller/EmployeeController.javaを編集

--- src/main/java/com/liso/springboot/controller/EmployeeController.java
+++ src/main/java/com/liso/springboot/controller/EmployeeController.java
@@ -1,5 +1,48 @@
 package com.liso.springboot.controller;
 
+import java.util.List;
+
+import com.liso.springboot.Pojos.CityRequest;
+import com.liso.springboot.Pojos.CourseRequest;
+import com.liso.springboot.entities.City;
+import com.liso.springboot.entities.Course;
+import com.liso.springboot.entities.Employee;
+import com.liso.springboot.services.CityService;
+import com.liso.springboot.services.CourseService;
+import com.liso.springboot.services.EmployeeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("employees/")
 public class EmployeeController {
 
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping("addemployees")
+    public List<Employee> addAllEmployees(@RequestBody List<Employee> employees) {
+        return employeeService.saveAllEmployees(employees);
+    }
+
+    @GetMapping("allemployees")
+    public List<Employee> getAllEmployees() {
+        return employeeService.findAllEmployees();
+    }
+
+    @GetMapping("employeeswithname")
+    public List<Employee> addAllEmployeesWithname(@RequestParam String employeename) {
+        return employeeService.findAllEmployeesByName(employeename);
+    }
+
+    @GetMapping("employeebyid")
+    public Employee getEmployeeById(@RequestParam int id) {
+        return employeeService.findAllEmployees(id);
+    }
 }
