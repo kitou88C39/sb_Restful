ae3ff22c56469dd02d260483c5046ac4270bde2a
kitou88C39 <kanfutrooper@icloud.com>
2023-09-07 09:24:15 +0900

    entities/Country.javaを編集

--- src/main/java/com/liso/springboot/entities/Country.java
+++ src/main/java/com/liso/springboot/entities/Country.java
@@ -6,50 +6,57 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GeneratedType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 import javax.persistence.OneToMany;
 import javax.persistence.JsonIgnore;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import org.springframework.lang.NonNull;
 
 @Entity
 @Table(name = "countries")
 
 public class Country {
-    
+
     @Id
-    @Column(name = "country_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "country_id")
     private Long id;
-    
+
+    @NonNull
     @Column(name = "country_name")
     private String countryname;
 
+    @NonNull
+    private long population;
+
     @JsonIgnore
     @OneToMany
-    @JoinColumn(name = "country_id")
+    @JoinColumn(name = "country_name")
     private Set<City> cities;
 
-    public Long getId(){
+    public Long getId() {
         return id;
     }
 
-    public void setId(Long id){
+    public void setId(Long id) {
         this.id = id;
     }
-    
-    public String getCountryname(){
+
+    public String getCountryname() {
         return countryname;
     }
 
-    public void setCountryname(String countryname){
+    public void setCountryname(String countryname) {
         this.countryname = countryname;
     }
 
-    public Set<City> getCities(){
+    public Set<City> getCities() {
         return cities;
     }
 
-    public void setCities(Set<City> cities){
+    public void setCities(Set<City> cities) {
         this.cities = cities;
     }
 }
-
