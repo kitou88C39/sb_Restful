d9d29eeb014a2244560c7c7e202f5bbca2bd5e6c
kitou88C39 <kanfutrooper@icloud.com>
2023-09-09 14:44:58 +0900

    repositories/CountryRepository.javaを編集

--- src/main/java/com/liso/springboot/repositories/CountryRepository.java
+++ src/main/java/com/liso/springboot/repositories/CountryRepository.java
@@ -2,25 +2,30 @@ package com.liso.springboot.repositories;
 
 import org.springframework.stereotype.Repository;
 
+import com.liso.springboot.entities.Country;
+
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Integer> {
 
     Country findById(int id);
 
-    List<Country> findByCountrynameAndPopulationGreaterThanEqual(String coutryname, long population);
+    @Query("from Country where id = ?1")
+    Country getById(int id);
 
-    List<Country> findByCountrynameAndPopulationLessThanEqual(String coutryname, long population);
+    List<Country> findByCountrynameAndPopulationGreaterThanEqual(String countryname, long population);
 
-    List<Country> findByCountrynameAndPopulationLessThan(String coutryname, long population);
+    List<Country> findByCountrynameAndPopulationLessThanEqual(String countryname, long population);
 
-    List<Country> findByCountrynameAndPopulationGreaterThan(String coutryname, long population);
+    List<Country> findByCountrynameAndPopulationLessThan(String countryname, long population);
+
+    List<Country> findByCountrynameAndPopulationGreaterThan(String countryname, long population);
 
     List<Country> findByCountrynameStartsWithOrderByPopulationAsc(String prefix);
 
     @Query("select c from Country c where c.countryname like ?1% ORDER BY c.population ASC")
     List<Country> getByCountryname(String prefix);
 
-    @Query("select c from Country c where c.countryname like :countryname% and c.population >= :population")
+    @Query("select c.countryname,c.population from Country c where c.countryname like :countryname% and c.population >= :population")
     List<Country> findByCountrynameAndPopulation(@Param("countryname") String countryname,
             @Param("poplatuin") long population);
 
