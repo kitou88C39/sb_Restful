ceab471b7ae89b724fd54c1dbeb8960faaf5ef8f
kitou88C39 <kanfutrooper@icloud.com>
2023-09-09 11:31:46 +0900

    controller/HomeController.javaを編集

--- src/main/java/com/liso/springboot/controller/HomeController.java
+++ src/main/java/com/liso/springboot/controller/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -73,7 +74,7 @@ public class HomeController {
     }
 
     @GetMapping("getcountrybyname")
-    public List<Country> getCountryByName(@RequestBody String prefix) {
-        return cityService.getCountryByName(prefix);
+    public List<Country> getCountryByNameandPop(@RequestParam String prefix, @RequestParam long population) {
+        return cityService.getCountryByNameandPop(prefix, population);
     }
 }
