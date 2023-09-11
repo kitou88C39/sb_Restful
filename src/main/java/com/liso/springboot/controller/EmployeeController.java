faeebbcdd79df7a7de2d9778baf5e16c660a7718
kitou88C39 <kanfutrooper@icloud.com>
2023-09-11 09:54:36 +0900

    controller/HomeController.javaを編集

--- src/main/java/com/liso/springboot/controller/EmployeeController.java
+++ src/main/java/com/liso/springboot/controller/EmployeeController.java
@@ -15,6 +15,7 @@ import com.liso.springboot.services.EmployeeService;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("employees/")
+@Validated
 public class EmployeeController {
 
     @Autowired
