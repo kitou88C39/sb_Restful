6b165f846a4578412b7b3aca2303b2730360cbf6
kitou88C39 <kanfutrooper@icloud.com>
2023-09-12 09:43:41 +0900

    common/GlobalExceptionHandler.javaを編集

--- src/main/java/com/liso/springboot/common/GlobalExceptionHandler.java
+++ src/main/java/com/liso/springboot/common/GlobalExceptionHandler.java
@@ -1,14 +1,21 @@
 package com.liso.springboot.common;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import jakarta.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -33,8 +40,15 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotReadable(MethodArgumentNotValidException ex,
             HttpHeaders headers, HttpStatus status, WebRequest request) {
-                Map<String, Object> body = new HashMap<>();
-                body.put("erros", ex.getMessage());
+        Map<String, Object> body = new HashMap<>();
+        body.put("erros", ex.getMessage());
 
         return new ResponseEntity<>(body, headers, status);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<String> handleConstrainValidationException(ConstraintViolationException ex) {
+        return new ResponseEntity<>("Error:" + ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
